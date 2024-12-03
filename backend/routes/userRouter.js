90c4b6b3e4c3ea4c3000e65053ba9df87e5a6c84
EK-806 <emilijakacakova236@gmail.com>
2024-12-03 10:40:26 +0100

    Update userRouter.js

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -13,7 +13,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
-router.get("/myprofile", isAuthenticated, getMyProfile);
+router.get("/myprofile", isAuthenticated, isAuthorized("Author"), getMyProfile);
 router.get("/authors", getAllAuthors);
 
 export default router;
