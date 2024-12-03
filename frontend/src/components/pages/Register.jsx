de2d3bdbcab99b4a8c79e31771ac43bbdf66f439
EK-806 <emilijakacakova236@gmail.com>
2024-12-03 10:35:33 +0100

    Update Register.jsx

--- frontend/src/components/pages/Register.jsx
+++ frontend/src/components/pages/Register.jsx
@@ -1,117 +1,146 @@
-import React, { useEffect, useState, useContext } from "react";
-import { useLocation, Link } from "react-router-dom";
-import axios from "axios";
-import { BeatLoader } from "react-spinners";
+import React, { useContext, useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { Context } from "../../main";
+import axios from "axios";
+import toast from "react-hot-toast";
 
-const Posts = () => {
-  const { mode } = useContext(Context);
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [visibleCount, setVisibleCount] = useState(6);
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const category = queryParams.get("category");
-  const authorUserName = queryParams.get("author");
-  const searchQuery = queryParams.get("search");
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
+  const [profilePicturePreview, setProfilePicturePreview] = useState("");
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setLoading(true);
-      try {
-        let url = "http://localhost:4000/api/v1/post/all";
-
-        if (category) {
-          url = `http://localhost:4000/api/v1/post/search/category/${category}`;
-        } else if (authorUserName) {
-          url = `http://localhost:4000/api/v1/post/search/author/${authorUserName}`;
-        } else if (searchQuery) {
-          url = `http://localhost:4000/api/v1/post/search/title/${searchQuery}`;
-        }
-
-        const { data } = await axios.get(url);
-        setPosts(data.posts || data.allPosts);
-      } catch (error) {
-        setError("Failed to fetch posts");
-      } finally {
-        setLoading(false);
-      }
+  const changeProfilePictureHandler = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      setProfilePicturePreview(reader.result);
+      setProfilePicture(file);
     };
-
-    fetchPosts();
-  }, [category, authorUserName, searchQuery]);
-
-  if (loading)
-    return (
-      <div className="loader-container">
-        <BeatLoader color="gray" size={30} />
-      </div>
-    );
-  if (error) return <p>{error}</p>;
-
-  const sortedPosts = posts?.sort(
-    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
-  );
-
-  const handleShowMore = () => {
-    setVisibleCount(visibleCount + 3);
   };
+
+  const { mode, isAuthenticated } = useContext(Context);
+
+  const navigateTo = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("phone", phone);
+    formData.append("password", password);
+    formData.append("role", role);
+    formData.append("profilePicture", profilePicture);
+
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/user/register",
+        formData,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      setName("");
+      setEmail("");
+      setPassword("");
+      setPhone("");
+      setRole("");
+      setProfilePicture("");
+      setProfilePicturePreview("");
+      toast.success(data.message);
+      navigateTo("/");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
 
   return (
     <article className={mode === "dark" ? "dark-bg" : "light-bg"}>
-      <h2>
-        {category
-          ? `Posts Related To Category: ${
-              category.charAt(0).toUpperCase() + category.slice(1)
-            }`
-          : authorUserName
-          ? `Posts by Author: ${
-              authorUserName.charAt(0).toUpperCase() + authorUserName.slice(1)
-            }`
-          : searchQuery
-          ? `Search Results for Title: ${searchQuery}`
-          : "All Posts"}
-      </h2>
-      <section className="posts">
-        <div className="container">
-          {sortedPosts && sortedPosts.length > 0 ? (
-            sortedPosts.slice(0, visibleCount).map((post) => (
-              <Link to={`/post/${post._id}`} className="card" key={post._id}>
-                <img
-                  src={post.mainImage.url}
-                  alt="post"
-                  className="post-image"
-                />
-                <span className="category">{post.category}</span>
-                <h4>{post.title}</h4>
-                <div className="authors_section">
-                  <div className="author">
-                    <img
-                      src={post.authorProfilePicture}
-                      alt="author_profilePicture"
-                      className="author-profile-image"
-                    />
-                    <p>{post.authorUserName}</p>
-                  </div>
-                </div>
-              </Link>
-            ))
-          ) : (
-            <div className="no-posts-message">
-              <p>No posts were found.</p>
+      <section className="auth-form">
+        <form onSubmit={handleRegister}>
+          <h1>Register</h1>
+          <div>
+            <input
+              type="text"
+              placeholder="UserName"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="Phone Number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <div className="ProfilePicturePlaceHolder">
+              <img
+                src={
+                  profilePicturePreview
+                    ? `${profilePicturePreview}`
+                    : "/imgProfPicPrev.png"
+                }
+                alt="profilePicture"
+              />
             </div>
-          )}
-        </div>
-
-        {sortedPosts && visibleCount < sortedPosts.length && (
-          <button className="show-more" onClick={handleShowMore}>
-            Show More
+            <input
+              type="file"
+              onChange={changeProfilePictureHandler}
+              style={{ border: "none" }}
+            />
+          </div>
+          <select value={role} onChange={(e) => setRole(e.target.value)}>
+            <option value="Select Role">Select Role</option>
+            <option value="Reader">Reader</option>
+            <option value="Author">Author</option>
+          </select>
+          <button className="submit-btn" type="submit">
+            Register
           </button>
-        )}
+          <p>
+            Are you already Registered? <Link to={"/login"}>Login Now</Link>
+          </p>
+        </form>
       </section>
     </article>
   );
 };
 
-export default Posts;
+export default Register;
